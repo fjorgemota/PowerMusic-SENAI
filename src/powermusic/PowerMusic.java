009c1332b46a7db41fc62e807a79339cbc9e8d4f
unknown <HP@Najuan.(none)>
2012-04-10 14:29:42 -0300

    teste do najuan2

--- src/powermusic/PowerMusic.java
+++ src/powermusic/PowerMusic.java
@@ -1,19 +1,18 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package powermusic;
 
-/**
- *
- * @author fernando_mota
- */
+import javaPlay.GameEngine;
+
+
 public class PowerMusic {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+       //Fase umaFase = new Fase();
+        //GameEngine.getInstance().addGameStateController( 1 , umaFase);
+
+        //GameEngine.getInstance().setStartingGameStateController( 1 );
+
+        GameEngine.getInstance().setFramesPerSecond( 30 );
+        GameEngine.getInstance().run();
     }
 }
