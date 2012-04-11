41153db29dfc8a6b56254b5793693c2c7ad1e786
unknown <HP@Najuan.(none)>
2012-04-11 16:28:21 -0300

    Tela inicial do jogo

--- src/powermusic/PowerMusic.java
+++ src/powermusic/PowerMusic.java
@@ -1,34 +1,27 @@
-
 package powermusic;
-<<<<<<< HEAD
-import javaPlay.GameEngine;
-/**
- *
- * @author fernando_mota
- */
-=======
 
 import javaPlay.GameEngine;
 
 
->>>>>>> 009c1332b46a7db41fc62e807a79339cbc9e8d4f
+import javaPlay.GameEngine;
+
 public class PowerMusic {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         //Fase umaFase = new Fase();       
         //GameEngine.getInstance().addGameStateController( 1 , umaFase);
-        
+
         //GameEngine.getInstance().setStartingGameStateController( 1 );
-        
-=======
-       //Fase umaFase = new Fase();
+
+
+        //Fase umaFase = new Fase();
         //GameEngine.getInstance().addGameStateController( 1 , umaFase);
 
         //GameEngine.getInstance().setStartingGameStateController( 1 );
 
->>>>>>> 009c1332b46a7db41fc62e807a79339cbc9e8d4f
-        GameEngine.getInstance().setFramesPerSecond( 30 );
+
+        GameEngine.getInstance().setFramesPerSecond(30);
         GameEngine.getInstance().run();
     }
 }
