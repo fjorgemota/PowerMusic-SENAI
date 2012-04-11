dc89fcbead353c54ae930e2b34ad9d80370bfaec
Najuan <na.juan@hotmail.com>
2012-04-11 16:35:21 -0300

    adicao de cenario

---
+++ src/powermusic/TelaInicial.java
@@ -0,0 +1,27 @@
+package powermusic;
+
+import java.awt.Graphics;
+import javaPlay.GameObject;
+import javaPlay.GameStateController;
+
+public class TelaInicial implements GameStateController {
+
+    public void load() {
+        
+    }
+
+    public void unload() {
+    }
+
+    public void start() {
+    }
+
+    public void step(long timeElapsed) {
+    }
+
+    public void draw(Graphics g) {
+    }
+
+    public void stop() {
+    }
+}
