3343f59d48c3ab6c49afe7bcdd362fce26da7158
Fernando Jorge Mota <f.j.mota13@gmail.com>
2012-04-13 14:21:19 -0300

    Adicao da fase de Ajuda

---
+++ src/powermusic/Ajuda.java
@@ -0,0 +1,42 @@
+package powermusic;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import javaPlay.GameObject;
+import javaPlay.GameStateController;
+import javaPlay.Mouse;
+import javaPlayExtras.Imagem;
+import javax.swing.JOptionPane;
+import utilidades.Utilidades;
+
+public class Ajuda implements GameStateController {
+
+    private Imagem bgImage;
+    private Mouse mouse;
+
+    public void load() {
+        try {
+            this.bgImage = new Imagem("img_cenario/logicamentefeito.png");
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+        }
+
+    }
+
+    public void unload() {
+    }
+
+    public void start() {
+    }
+
+    public void step(long timeElapsed) {
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.black);
+        g.fillRect(0, 0, 3000, 2400);
+    }
+
+    public void stop() {
+    }
+}
