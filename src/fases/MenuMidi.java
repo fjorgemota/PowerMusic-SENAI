f02a9785513a65ff668508740f508a1bedef64af
unknown <Samara@Samara-PC.(none)>
2012-04-19 15:32:38 -0300

    Alterap no menu midi

--- src/fases/MenuMidi.java
+++ src/fases/MenuMidi.java
@@ -15,7 +15,7 @@ public class MenuMidi implements GameStateController {
 
     @Override
     public void load() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        
     }
 
     @Override
