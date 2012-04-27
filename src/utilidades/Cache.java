ed9990b816e7f974444fc170a75b72ea17caa816
Najuan <na.juan@hotmail.com>
2012-04-26 23:23:28 -0300

    Correcao de alguns arquivos

--- src/utilidades/Cache.java
+++ src/utilidades/Cache.java
@@ -1,16 +1,10 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package utilidades;
 
 import java.util.HashMap;
 
-/**
- *
- * @author fernando_mota
- */
+
 public class Cache {
     public static HashMap<Object, Object> data;
-    public 
+
 }
