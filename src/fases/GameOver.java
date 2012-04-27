df06d6d3b9b1caa51a43a408de53f0ede95dd59b
Fernando Jorge Mota <f.j.mota13@gmail.com>
2012-04-27 19:29:46 -0300

    Implementacao de fase de GameOver

--- src/fases/GameOver.java
+++ src/fases/GameOver.java
@@ -4,10 +4,46 @@
  */
 package fases;
 
+import java.awt.Graphics;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javaPlay.GameEngine;
+import javaPlay.GameStateController;
+import javaPlayExtras.Imagem;
+import utilidades.Utilidades;
+
 /**
  *
  * @author Samara
  */
-public class GameOver {
+public class GameOver implements GameStateController{
+    private Imagem imagem;
+    public void load() {
+        try {
+            this.imagem = new Imagem("img_cenario/gameover.png");
+        } catch (Exception ex) {
+            Utilidades.alertar("Erro ao carregar a imagem de GameOver: "+ex.getMessage());
+        }
+    }
+
+    public void unload() {
+    }
+
+    public void start() {
+        
+    }
+
+    public void step(long timeElapsed) {
+        if(Utilidades.estaClicandoEm(0, 0, 800, 620)){
+            GameEngine.getInstance().setNextGameStateController(2);
+        }
+    }
+
+    public void draw(Graphics g) {
+        this.imagem.draw(g, 0, 0);
+    }
+
+    public void stop() {
+    }
     
 }
