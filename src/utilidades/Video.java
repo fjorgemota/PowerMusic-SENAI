fce798ab60ea801b8edb5257790684a23b8f9efa
Fernando Jorge Mota <f.j.mota13@gmail.com>
2012-04-27 14:51:12 -0300

    Implementacao de carregamento assincrono do video e coisas do genero

--- src/utilidades/Video.java
+++ src/utilidades/Video.java
@@ -8,42 +8,66 @@ import java.awt.Component;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
+import javax.media.ControllerEvent;
+import javax.media.ControllerListener;
 import javax.media.Format;
 import javax.media.Manager;
 import javax.media.Player;
 import javax.media.PlugInManager;
 import net.sourceforge.jffmpeg.AudioDecoder;
 import net.sourceforge.jffmpeg.VideoDecoder;
+import sun.awt.PlatformFont;
 
 /**
  *
  * @author fernando_mota
  */
-public class Video{
+public class Video implements ControllerListener, Runnable{
     private String filename;
-    Player player = null;
+    private Player player = null;
+    private boolean canPlay;
+    private Thread theThread;
     public Video(String filename){
+        super();
         this.filename = filename;
         this.install();
         this.load();
     }
     public URL getURL(){
         try {
-            return new URL("file://"+System.getProperty("user.dir")+"/videos/"+this.filename);
+            System.out.println("Criando URL para o player "+this.filename);
+            URL url = new URL("file://"+System.getProperty("user.dir")+"/videos/"+this.filename);
+            System.out.println("URL criado para o player "+this.filename);
+            return url;
         } catch (Exception ex) {
             Utilidades.alertar("Alerta 2:"+ex.getMessage());
             return null;
         }
     }
     public void load(){
+        System.out.println("Executando Thread..");
+        theThread = new Thread(this);
+        theThread.setDaemon(true);
+        theThread.setPriority(Thread.MAX_PRIORITY);
+        theThread.start();
+        System.out.println("Terminou de executar a Thread");
+    }
+    public void run(){
         try {
-            player = Manager.createRealizedPlayer(this.getURL());
-            player.realize();
+            System.out.println("Criando player "+this.filename);
+            this.player = Manager.createPlayer(this.getURL());
+            System.out.println("Adicionando controlador ao player "+this.filename);
+            this.player.addControllerListener(this);
+            System.out.println("Realizando player "+this.filename);
+            this.player.realize();
         } catch (Exception ex) {
             Utilidades.alertar("Alerta 3:"+ex.getMessage()+" with file "+this.filename);
         }
     }
     public Component getSwingComponent(){
+        while(!this.canPlay){
+            continue;
+        }
         return player.getVisualComponent();
     }
     public void setVolume(float volume){
@@ -90,10 +114,26 @@ public class Video{
     public void play(){
         this.player.start();
     }
-    public void stop(){
+    public void pause(){
         this.player.stop();
     }
-    public void close(){
-        this.player.deallocate();
+
+    public void controllerUpdate(ControllerEvent ce) {
+        switch(this.player.getState()){
+            case Player.Realized:
+                System.out.println("Player "+this.filename+" Realizado!");
+                //this.player.prefetch();
+                this.canPlay = true;
+                break;
+            case Player.Prefetched:
+                System.out.println("Player "+this.filename+" pre-capturado!");
+                break;
+            case Player.Realizing:
+                System.out.println("Realizando Player "+this.filename+" ..");
+                break;
+            case Player.Prefetching:
+                System.out.println("Pre-capturando Player "+this.filename+" ...");
+                break;             
+        }
     }
 }
