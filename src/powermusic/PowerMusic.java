6c275c12e1d5faa095c3f8fec3aceb16efa1073b
Fernando Jorge Mota <f.j.mota13@gmail.com>
2012-04-29 01:18:00 -0300

    Adicao de fase para leitura de MIDI personalizado

--- src/powermusic/PowerMusic.java
+++ src/powermusic/PowerMusic.java
@@ -4,31 +4,7 @@
  */
 package powermusic;
 
-import fases.MenuFases;
-import fases.TelaInicial;
-import fases.Ajuda;
-import fases.FaseEasy1;
-import fases.FaseEasy2;
-import fases.FaseEasy3;
-
-import fases.FaseHard2;
-import fases.FaseHard1;
-import fases.FaseHard3;
-import fases.FaseMedium1;
-import fases.FaseMedium2;
-import fases.FaseMedium3;
-import fases.GameOver;
-import fases.IntervaloHard1;
-import fases.IntervaloHard2;
-import fases.IntervaloHard3;
-import fases.MenuMidi;
-import fases.IntervaloInicial;
-import fases.IntervaloInicial2;
-import fases.IntervaloInicial3;
-import fases.IntervaloMedium1;
-import fases.IntervaloMedium2;
-import fases.IntervaloMedium3;
-import fases.GameOver;
+import fases.*;
 import javaPlay.GameEngine;
 
 /**
@@ -65,12 +41,10 @@ public class PowerMusic {
         GameEngine.getInstance().addGameStateController(21, new IntervaloHard1());
         GameEngine.getInstance().addGameStateController(22, new IntervaloHard2());
         GameEngine.getInstance().addGameStateController(23, new IntervaloHard3());
-        GameEngine.getInstance().addGameStateController(24, new GameOver());
-
+        GameEngine.getInstance().addGameStateController(24, GameOver.getInstance());
+        GameEngine.getInstance().addGameStateController(25, MusicMIDI.getInstance());
         GameEngine.getInstance().setStartingGameStateController(2);
-
-        GameEngine.getInstance().setFramesPerSecond(10);
-
+        GameEngine.getInstance().setFramesPerSecond(200);
         GameEngine.getInstance().run();
     }
 }
